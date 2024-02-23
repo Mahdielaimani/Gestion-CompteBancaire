cf955302825a40053a117d292a1cb6d0522fc4a9
Mahdi <mahdielaimani@gmail.com>
2024-02-23 20:15:54 +0100

    Implemented Dashboard Operations for Admin and Client; Initiated Functional Logic Development
    This commit marks the implementation of dashboard operations for both administrators and clients, along with the initiation of functional logic development:

--- auth/signin.py
+++ auth/signin.py
@@ -1,9 +1,9 @@
-import auth.signin_admin
+import admin.signin_admin
 import auth.signin_client
 def sign_in():
     role = input("Are you logging in as an administrator or a client? (admin/client): ").lower()
     if role == 'admin':
-        auth.signin_admin.sign_in_admin()
+        admin.signin_admin.sign_in_admin()
     elif role == 'client':
         auth.signin_client.sign_in_client()
     else:
--- view/dashboard_admin.py
+++ view/dashboard_admin.py
@@ -24,3 +24,23 @@ def dashboard_admin():
         display_client_data()
      case _:
         print("Invalid admin operation key.")
+
+# import functions.admin_operations
+
+# def dashboard_admin():
+#     print("Available admin operations:")
+#     print("1. Add client")
+#     print("2. Delete client")
+#     print("3. Display client data")
+#     admin_operation_key = input("Enter operation Number: ")
+
+#     match admin_operation_key:
+#      case "1":
+#         functions.admin_operations.add_client()
+        
+#      case "2":
+#         functions.admin_operations.delete_client()
+#      case "3":
+#         functions.admin_operations.display_client_data()
+#      case _:
+#         print("Invalid admin operation key.")
