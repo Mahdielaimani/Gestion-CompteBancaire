54990651169d4bc0ff9a1fbfccfe921dafd6931e
Mahdi <mahdielaimani@gmail.com>
2024-02-24 00:25:04 +0100

    Completed Admin Operations: Client Management and Transaction Tracking
    This commit finalizes the operations available to administrators:
    
    Client Addition:
    
    Implemented functionality to create a new client profile.
    Enabled input of personal information including name, address, email, etc.
    Provided the capability to set login credentials for the client.
    Incorporated the feature to associate the client with one or multiple accounts.
    Client Deletion:
    
    Added functionality to delete a client's profile.
    Implemented the removal of personal information and associated accounts for the client.
    Client Data Management and Transaction Tracking:
    
    Implemented the functionality to display all client data.
    Enabled administrators to track client transactions.
    Provided the capability to modify client personal information, login credentials, and associated accounts.

--- view/dashboard_admin.py
+++ view/dashboard_admin.py
@@ -1,46 +1,21 @@
-def add_client():
-    print("Add a client")
 
-def delete_client():
-    print("Delete a client")
-
-def display_client_data():
-    print("Display client data")
+import functions.admin_operations
 
 def dashboard_admin():
     print("Available admin operations:")
     print("1. Add client")
     print("2. Delete client")
     print("3. Display client data")
-
     admin_operation_key = input("Enter operation Number: ")
 
     match admin_operation_key:
      case "1":
-        add_client()
+        functions.admin_operations.add_client()
      case "2":
-        delete_client()
+        name = input("Enter user's name to delete: ")
+        email = input("Enter user's email to delete: ")
+        functions.admin_operations.delete_client(name, email)
      case "3":
-        display_client_data()
+          functions.admin_operations.show_clients_data()
      case _:
         print("Invalid admin operation key.")
-
-# import functions.admin_operations
-
-# def dashboard_admin():
-#     print("Available admin operations:")
-#     print("1. Add client")
-#     print("2. Delete client")
-#     print("3. Display client data")
-#     admin_operation_key = input("Enter operation Number: ")
-
-#     match admin_operation_key:
-#      case "1":
-#         functions.admin_operations.add_client()
-        
-#      case "2":
-#         functions.admin_operations.delete_client()
-#      case "3":
-#         functions.admin_operations.display_client_data()
-#      case _:
-#         print("Invalid admin operation key.")
